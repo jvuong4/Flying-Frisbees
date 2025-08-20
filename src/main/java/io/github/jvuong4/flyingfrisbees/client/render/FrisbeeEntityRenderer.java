d7ef2c700730b54513fc5ee514a2b07793b0a134
jvuong4 <itsjaevy@yahoo.com>
2025-08-19 20:58:30 -0500

    slight tweaks :^)
    - removed some unnecessary comments
    - tweaked frisbee physics: less speed and less gravity so it's easier to catch and goes further
    - frisbee can be picked up even outside of creative
    - when retrieving, only checks if the target is sneaking if said target is a player

--- src/main/java/io/github/jvuong4/flyingfrisbees/client/render/FrisbeeEntityRenderer.java
+++ src/main/java/io/github/jvuong4/flyingfrisbees/client/render/FrisbeeEntityRenderer.java
@@ -22,26 +22,7 @@ public class FrisbeeEntityRenderer<R extends EntityRenderState & GeoRenderState>
 
 	public FrisbeeEntityRenderer(EntityRendererFactory.Context context) {
 		super(context, new FrisbeeEntityModel());
-		/*
-		//this uses <String,Identifier>
-		frisbeeTextures.put("frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/frisbee.png"));
-		frisbeeTextures.put("black_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/black_frisbee.png"));
-		frisbeeTextures.put("blue_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/blue_frisbee.png"));
-		frisbeeTextures.put("brown_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/brown_frisbee.png"));
-		frisbeeTextures.put("cyan_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/cyan_frisbee.png"));
-		frisbeeTextures.put("gray_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/gray_frisbee.png"));
-		frisbeeTextures.put("green_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/green_frisbee.png"));
-		frisbeeTextures.put("light_blue_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/light_blue_frisbee.png"));
-		frisbeeTextures.put("light_gray_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/light_gray_frisbee.png"));
-		frisbeeTextures.put("lime_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/lime_frisbee.png"));
-		frisbeeTextures.put("magenta_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/magenta_frisbee.png"));
-		frisbeeTextures.put("orange_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/orange_frisbee.png"));
-		frisbeeTextures.put("pink_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/pink_frisbee.png"));
-		frisbeeTextures.put("purple_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/purple_frisbee.png"));
-		frisbeeTextures.put("red_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/red_frisbee.png"));
-		frisbeeTextures.put("white_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/white_frisbee.png"));
-		frisbeeTextures.put("yellow_frisbee",Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/yellow_frisbee.png"));
-		*/
+
 		frisbeeTextures.put(FlyingFrisbeesItems.FRISBEE,Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/frisbee.png"));
 		frisbeeTextures.put(FlyingFrisbeesItems.BLACK_FRISBEE,Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/black_frisbee.png"));
 		frisbeeTextures.put(FlyingFrisbeesItems.BLUE_FRISBEE,Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/blue_frisbee.png"));
@@ -60,25 +41,7 @@ public class FrisbeeEntityRenderer<R extends EntityRenderState & GeoRenderState>
 		frisbeeTextures.put(FlyingFrisbeesItems.WHITE_FRISBEE,Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/white_frisbee.png"));
 		frisbeeTextures.put(FlyingFrisbeesItems.YELLOW_FRISBEE,Identifier.of(FlyingFrisbees.MOD_ID, "textures/entity/yellow_frisbee.png"));
 
-
-		//, new FrisbeeEntityModel()); //, new FrisbeeEntityModel(context.getPart(FrisbeeEntityModel.FRISBEE)), 0.5f);
 	}
-/*
-	public Identifier getTexture(FrisbeeEntityRenderState FrisbeeEntityRenderState) {
-		if (foxEntityRenderState.type == FoxEntity.Variant.RED) {
-			return foxEntityRenderState.sleeping ? SLEEPING_TEXTURE : TEXTURE;
-		} else {
-			return foxEntityRenderState.sleeping ? SLEEPING_SNOW_TEXTURE : SNOW_TEXTURE;
-		}
-	}
-	*/
-
-	/*
-	@Override
- 	public void addRenderData(FrisbeeEntity animatable, RenderData relatedObject, R renderState) {
-
-	}
-	 */
 
 	DataTicket<String> TEXTURE = DataTicket.create("frisbee_texture", String.class);
 
